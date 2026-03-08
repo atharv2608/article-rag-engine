e4e46334b044ab4f70d3527c20a39c3d975ae78a
Atharv Lingayat <atharv.lingayat@contentstack.com>
2026-03-08 17:11:12 +0530

    feat: enhance retrieval function to include minimum similarity filtering and detailed logging

--- src/services/retrievalService.js
+++ src/services/retrievalService.js
@@ -1,16 +1,23 @@
 import { getCollection } from "../db/chroma.js";
 import { createEmbedding } from "../utils/createEmbedding.js";
 
-export const retrieveRelevantChunks = async (question, topK = 3) => {
-
+export const retrieveRelevantChunks = async (question, topK = 3, minSimilarity = 0.7) => {
     const collection = await getCollection();
-
     const queryEmbedding = await createEmbedding(question);
 
     const results = await collection.query({
         queryEmbeddings: [queryEmbedding],
-        nResults: topK
+        nResults: topK,
+        include: ["documents", "distances"]
     });
-
-    return results.documents[0];
+    const threshold = 1 - minSimilarity;
+    
+    const filteredDocs = [];
+    for (let i = 0; i < results.documents[0].length; i++) {
+        if (results.distances[0][i] <= threshold) {
+            filteredDocs.push(results.documents[0][i]);
+        }
+    }
+    
+    return filteredDocs;
 };
